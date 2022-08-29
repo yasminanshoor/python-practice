508d53de3d6dfe652c813874b4ce49df88e10b5d
Yasmin <yasminanshoor@gmail.com>
2022-08-29 17:10:16 +0100

    length of variables

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-print("This is a test file")
-name = 'yasmin'
-surname = 'Anshoor'
-# age = 25
-# #print(age)
-# length = len(surname)
-print(f'{name} {surname}')
+#print("This is a test file")
+name = 'yusra'
+surname = 'anshoor'
+print(name+ ' ' +surname)
+length = len(name+surname)
+print(name+surname)
